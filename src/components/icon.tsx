cbd0c99175b9dab8252d8cad762a35097e142ac1
Suraj <suraj.ukumar.p@gmail.com>
2024-12-01 21:53:40 +0530

    fix: error fixes.

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,8 +1,7 @@
-
 type IconProps = React.HTMLAttributes<SVGElement>;
 
 export const Icons = {
-  logo: (props: IconProps) => (
+  logo: () => (
     <svg
       width="25"
       height="25"
@@ -10,13 +9,13 @@ export const Icons = {
       fill="white"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect width="32" height="32" rx="6"  />
+      <rect width="32" height="32" rx="6" />
       <g opacity="0.8">
         <rect x="8" y="10" width="2" height="12" fill="black" />
-        <rect x="12" y="8" width="2" height="16"  fill="black" />
+        <rect x="12" y="8" width="2" height="16" fill="black" />
         <rect x="16" y="6" width="2" height="20" fill="black" />
         <rect x="20" y="8" width="2" height="16" fill="black" />
-        <rect x="24" y="10" width="2" height="12" fill="black"/>
+        <rect x="24" y="10" width="2" height="12" fill="black" />
       </g>
       <defs>
         <linearGradient
